ef40899713787d2ebe8e16dc28d4940117a55560
lucky99ethan <ethansawe99@gmail.com>
2024-10-07 11:52:55 +0300

    initial stage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import html2canvas from 'html2canvas';
-import pkg from 'file-saver';
-const { saveAs } = pkg;
+// import pkg from 'file-saver';
+// const { saveAs } = pkg;
+import './App.css';
 
 const cars = [
   {
